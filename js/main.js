988e706e012243f1f9b535a4799d786915386627
Alex <astolz460@gmail.com>
2018-03-18 18:50:02 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+function getSelectSubject(){
+    var index = document.getElementById("subjectList").value ;
+    document.getElementById("sub").innerHTML = index;
+}
+getSelectSubject();
